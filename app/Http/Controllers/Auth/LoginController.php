bf641cd5066a4e2f5bc972b14bca2cfa9db4ee12
developer-tamim <tawsiftamim24@gmail.com>
2024-04-19 15:33:45 +0600

    login authentication api

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
@@ -11,10 +15,17 @@ class LoginController extends Controller
      * Handle the incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    //  @return \Illuminate\Http\Response
+
+    public function __invoke(LoginRequest $request)
     {
-        //
+        $user = User::where('email', $request->email)->first();
+
+        if(!$user || !Hash::check($request->password, $user->password)){
+            throw ValidationException::withMessages([
+                'email' => ['The credentials you entered are incorrect.'],
+            ]);
+        }
     }
 }
